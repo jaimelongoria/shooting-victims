7e546126ff42faea22cea8b2b94b151359e45e9e
Jaime Longoria <jaime.longoria@journalism.cuny.edu>
2019-03-24 22:35:52 -0400

    four data points with lodash

---
+++ data.js
@@ -0,0 +1,35 @@
+let result;
+//take the var data and its
+let data = [{
+		"boro": "the Bronx",
+		"precinct": 47,
+		"statistical_murder_flag": "FALSE",
+		"vic_age_group": "25-44",
+		"vic_sex": "male",
+		"vic_race": "Black"
+  },
+	{
+		"boro": "Manhattan",
+		"precinct": 6,
+		"statistical_murder_flag": "TRUE",
+		"vic_age_group": "45-64",
+		"vic_sex": "male",
+		"vic_race": "White"
+  },
+	{
+		"boro": "Brooklyn",
+		"precinct": 77,
+		"statistical_murder_flag": "FALSE",
+		"vic_age_group": "18-24",
+		"vic_sex": "male",
+		"vic_race": "Black"
+  },
+	{
+		"boro": "Queens",
+		"precinct": 114,
+		"statistical_murder_flag": "FALSE",
+		"vic_age_group": "25-44",
+		"vic_sex": "male",
+		"vic_race": "Black"
+  }
+];
