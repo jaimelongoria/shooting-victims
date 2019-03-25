bfd8d85582c44b7c8ffa255beb7881b6aa711602
Jaime Longoria <jaime.longoria@journalism.cuny.edu>
2019-03-25 00:37:05 -0400

    multiple buttons added, don't work

--- main.js
+++ main.js
@@ -5,19 +5,47 @@ $(document).ready(function () { // we must wait for the DOM to be ready as the b
 		option_template = _.template($("#select_template").html());
 
 	_.each(data, function (person) {
-		$("#character").append(option_template({ boro: person.boro }));
+		$("#character").append(option_template({ name: person.boro }));
+	});
+
+	_.each(data, function (person) {
+		$("#copjuris").append(option_template({ name: person.precinct }));
+	});
+
+	_.each(data, function (person) {
+		$("#killed").append(option_template({ name: person.statistical_murder_flag }));
+	});
+
+	_.each(data, function (person) {
+		$("#age").append(option_template({ name: person.vic_age_group }));
+	});
+
+	_.each(data, function (person) {
+		$("#gender").append(option_template({ name: person.vic_sex }));
+	});
+
+	_.each(data, function (person) {
+		$("#race").append(option_template({ name: person.vic_race }));
 	});
 
 	$('#enter').on("click", function (e) {
 		e.preventDefault(); //disable the button's default behavior
 		// DO STUFF...
 		persons_boro = $("#character").val();
-		result = _.find(data, { boro: persons_boro });
+		result = _.find(data, { name: persons_boro });
+		persons_precinct = $("#copjuris").val();
+		result = _.find(data, { name: persons_precinct });
 
 		console.log(result);
 
-		$("#favorites").html(person_template({ person: result }));
+		$("#shootings").html(person_template({ person: result }));
 	});
+
+
+
+
+
+
 
 
 
